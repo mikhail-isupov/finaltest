d189ad5df3a6b2430a320455ac32002786fe40a0
Mikhail Isupov <miknsk@gmail.com>
2022-10-02 22:57:16 +0700

    Добавлена программа, решающая поставленную задачу

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace final
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] inputArray = {"1", "12", "123", "1234"};
+
+            string[] outputArray = StringArrayFilter(inputArray);
+            
+            for (int i = 0; i < outputArray.Length; i++) Console.WriteLine(outputArray[i]);
+        }
+
+        static string[] StringArrayFilter(string[] inputArray, int maxLength = 3)
+        {
+            
+            int counter = 0;
+            
+            for (int i = 0; i < inputArray.Length; i++) if (inputArray[i].Length <= maxLength) counter +=1;
+
+            string[] outputArray = new string[counter];
+
+            counter = 0;
+
+            for (int i = 0; i < inputArray.Length; i++) if (inputArray[i].Length <= maxLength) outputArray[counter++] = inputArray[i];
+            
+            return outputArray;
+
+        }
+    }
+}
